48c08cb3e0b5bb64eb33490c2ab2afec77472648
Sokolov Denis <denis.sokolov48@gmail.com>
2021-05-04 14:47:01 +0300

    Добавил переключатель в режим симуляции

--- src/control_module/include/control_module/control.h
+++ src/control_module/include/control_module/control.h
@@ -6,24 +6,24 @@
 #include <std_srvs/Trigger.h>
 
 
-struct Module
+struct Sensor
 {
 
-  Module(bool state, std::string pkg, std::string node)
+  Sensor(bool state, QString pkg, QString node)
     : state_(state)
     , package_(pkg)
     , node_(node) {}
 
-  Module()
+  Sensor()
     : state_(0)
     , package_()
     , node_()
     , console_(new QProcess()) {}
 
   bool state_;
-  std::string package_;
-  std::string node_;
-  std::string name_;
+  QString package_;
+  QString node_;
+  QString name_;
   QProcess * console_;
 };
 
@@ -37,15 +37,16 @@ public:
   void update();
   void updateParams();
   void start();
-  void fillStruct(Module& struc, std::string prefix);
+  void fillStruct(Sensor& struc, QString prefix);
   bool callbackUpdate(std_srvs::Trigger::Request& request,
                       std_srvs::Trigger::Response& response);
 
 private:
-  std::vector<Module> modules_;
+  std::vector<Sensor> modules_;
   std::vector<std::string> names_;
   ros::ServiceServer srv_update_;
   ros::NodeHandle nh_;
+  bool simulator_;
 };
 
 
@@ -60,3 +61,5 @@ void readParam(const std::string param_name, T& param_value,
     param_value = default_value;
   }
 }
+
+
--- src/control_module/src/control.cpp
+++ src/control_module/src/control.cpp
@@ -1,16 +1,30 @@
 #include "control_module/control.h"
 
-
+template<>
+void readParam(const std::string param_name, QString& param_value,
+               const QString default_value)
+{
+  std::string tmp_str = param_value.toStdString();
+  if (!ros::param::get(param_name, tmp_str))
+  {
+    ROS_WARN_STREAM("Parameter \""
+                        << param_name << "\" didn' find in Parameter Server."
+                        << "\nSetting default value: " << &default_value);
+    param_value = default_value;
+  }
+  param_value = QString::fromStdString(tmp_str);
+}
 
 ControlModule::ControlModule()
   : nh_()
+  , simulator_(false)
 {
   srv_update_ = nh_.advertiseService("updateParams", &ControlModule::callbackUpdate, this);
   readParam("/gui_config/sensor_types", names_, names_);
   // init
   modules_.resize(names_.size());
   for (size_t i = 0; i < modules_.size(); i++)
-    modules_.at(i).name_ = names_.at(i);
+    modules_.at(i).name_ = QString::fromStdString(names_.at(i));
 
   updateParams();
 
@@ -37,23 +51,24 @@ ControlModule::~ControlModule()
 
 void ControlModule::updateParams()
 {
-  ros::param::del("/gui_config/");
+  bool delete_success = ros::param::del("/gui_config/");
   QProcess console;
   static QString pkg_path(ros::package::getPath("gui_control_system").c_str());
-  console.start("rosparam load " + pkg_path + "/config/qt_out.yaml");
+  console.start("rosparam", {"load", pkg_path + "/config/qt_out.yaml"});
   console.waitForFinished();
   for (auto& it : modules_)
   {
     fillStruct(it, "/gui_config/" + it.name_);
   }
+  readParam("/gui_config/simulation", simulator_, false);
 }
 
 
-void ControlModule::fillStruct(Module& struc, std::string prefix)
+void ControlModule::fillStruct(Sensor& struc, QString prefix)
 {
-  readParam(prefix + "/turn", struc.state_, false);
-  readParam(prefix + "/pkg", struc.package_, std::string("none"));
-  readParam(prefix + "/node", struc.node_, std::string("none"));
+  readParam(prefix.toStdString() + "/turn", struc.state_, false);
+  readParam(prefix.toStdString() + "/pkg", struc.package_, QString());
+  readParam(prefix.toStdString() + "/node", struc.node_, QString());
 }
 
 
@@ -61,10 +76,19 @@ void ControlModule::start()
 {
   for (auto it : modules_)
   {
-    std::string cmd("roslaunch control_module node_launcher.launch name:=" +
-                     it.node_ + "_name" + " pkg:=" + it.package_ + " type:=" + it.node_);
     if (it.state_)
-      it.console_->start(QString::fromStdString(cmd));
+    {
+      QStringList args = simulator_ ? QStringList{
+                                     "sensor_node", "start_node.launch",
+                                     "node_name:=" + it.node_ + "_autoname",
+                                     "subscribe_topic_name:=" + it.package_}
+                                   : QStringList{
+                                     "control_module", "node_launcher.launch",
+                                     "name:=" + it.node_ + "_autoname",
+                                     "pkg:=" + it.package_,
+                                     "type:=" + it.node_ };
+      it.console_->start("roslaunch", args);
+    }
   }
 }
 
@@ -75,17 +99,28 @@ void ControlModule::update()
   updateParams();
   for (size_t i = 0; i < modules_.size(); i++)
   {
-    std::string cmd("roslaunch control_module node_launcher.launch name:=" +
-                     modules_.at(i).node_ + "_name" + " pkg:=" + modules_.at(i).package_ +
-                    " type:=" + modules_.at(i).node_);
-
     // Если изменился статус
     if (modules_.at(i).state_ != old_modules.at(i).state_)
     {
       if (modules_.at(i).state_)
-        modules_.at(i).console_->start(QString::fromStdString(cmd));
+      {
+        QStringList args = simulator_ ? QStringList{
+                                       "sensor_node", "start_node.launch",
+                                       "node_name:=" + modules_.at(i).node_ + "_autoname",
+                                       "subscribe_topic_name:=" + modules_.at(i).package_}
+                                     : QStringList{
+                                       "control_module", "node_launcher.launch",
+                                       "name:=" + modules_.at(i).node_ + "_autoname",
+                                       "pkg:=" + modules_.at(i).package_,
+                                       "type:=" + modules_.at(i).node_ };
+        modules_.at(i).console_->start("roslaunch", args);
+      }
+
       else
+      {
         modules_.at(i).console_->terminate();
+        modules_.at(i).console_->waitForFinished();
+      }
     }
   }
 }
